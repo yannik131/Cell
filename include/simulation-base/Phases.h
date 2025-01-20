5c128896a6141709d878136964431886e2f93be6
yannik131 <yannik131@web.de>
2025-01-20 09:37:02 +0100

    Added phases include

---
+++ include/simulation-base/Phases.h
@@ -0,0 +1,28 @@
+#ifndef PHASES_H
+#define PHASES_H
+
+enum CellCyclePhases
+{
+    Interphase,
+    Mitosis
+};
+
+enum InterphasePhases
+{
+    G0,
+    G1,
+    S,
+    G2
+};
+
+enum MitosisPhases
+{
+    Prophase,
+    Prometaphase,
+    Metaphase,
+    Anaphase,
+    Telophase,
+    Cytokinesis
+};
+
+#endif /* PHASES_H */
