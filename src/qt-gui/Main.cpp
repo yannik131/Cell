66a9d8815af72e9aacf78ba2e36eeda9c74eefe5
Yannik Schroeder <yannik.schroeder@uni-bayreuth.de>
2025-02-09 01:34:37 +0100

    Main window .ui

--- src/qt-gui/Main.cpp
+++ src/qt-gui/Main.cpp
@@ -1,16 +1,35 @@
+#include "Logging.hpp"
+#include "MainWindow.hpp"
+
 #include <QApplication>
-#include <QWidget>
+#include <QMessageBox>
 
 #include <QtPlugin>
 Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
+    initLogging(argc, argv);
     QApplication app(argc, argv);
-    QWidget window;
-    window.setWindowTitle("Simple Qt Window");
-    window.resize(400, 300);
-    window.show();
 
-    return app.exec();
+    try
+    {
+        MainWindow mainWindow;
+        mainWindow.show();
+
+        return app.exec();
+    }
+
+    catch (const std::exception& e)
+    {
+        QMessageBox::critical(
+            nullptr, "Error",
+            QString("Unhandled exception occured: %1\nThis is probably a bug. Gotta shut down now :()").arg(e.what()));
+    }
+    catch (...)
+    {
+        QMessageBox::critical(nullptr, "Error", QString("An unknown exception occured! Closing down."));
+    }
+
+    return 1;
 }
