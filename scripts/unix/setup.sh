de4716e26a2168bdba62c34e005db59662e29328
yannik131 <yannik131@web.de>
2025-02-18 17:23:44 +0100

    Script for unix, gitignore for mac

--- scripts/unix/setup.sh
+++ scripts/unix/setup.sh
@@ -10,28 +10,37 @@ set -e
 cd "$(dirname "$0")/../../"
 
 # Check if contrib/vcpkg exists
-if [ ! -d "contrib/vcpkg/" ]; then
-    git submodule update
+if [ ! -d "contrib/vcpkg/buildtrees/qtbase" ]; then
+    git submodule update --init
     ./contrib/vcpkg/bootstrap-vcpkg.sh
-    ./contrib/vcpkg/vcpkg install glog sfml nanoflann qtbase
+fi
+
+#required on mac/linux:
+# autoconf autoconf-archive automake make libtool curl zip unzip pkgconfig
+
+export MACOSX_DEPLOYMENT_TARGET=14.0
+export VCPKG_KEEP_ENV_VARS=MACOSX_DEPLOYMENT_TARGET
+./contrib/vcpkg/vcpkg install glog sfml nanoflann qtbase
+
+CONFIGURE_OPTIONS=""
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    CONFIGURE_OPTIONS="-DCMAKE_OSX_ARCHITECTURES=arm64"
 fi
 
 # Release build
 if [ ! -d "build_release" ]; then
     mkdir build_release
     cd build_release
-    cmake .. -DCMAKE_BUILD_TYPE=Release
+    cmake .. -DCMAKE_BUILD_TYPE=Release $CONFIGURE_OPTIONS
     cmake --build . --config Release --parallel 4
     cd ..
-    ./contrib/vcpkg/installed/x64-windows/tools/Qt6/bin/windeployqt6.exe ./build_release/Release/qt-gui.exe
 fi
 
 # Debug build
 if [ ! -d "build_debug" ]; then
     mkdir build_debug
     cd build_debug
-    cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+    cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON $CONFIGURE_OPTIONS
     cmake --build . --config Debug --parallel 4
     cd ..
-    ./contrib/vcpkg/installed/x64-windows/tools/Qt6/bin/windeployqt.debug.bat ./build_debug/Debug/qt-gui.exe
 fi
