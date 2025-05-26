0c728f2aef8a9f3829dbd1f9391fdfd495ea74b0
Yannik Schroeder <yannik.schroeder@uni-bayreuth.de>
2025-05-26 20:01:02 +0200

    windows crap

---
+++ src/benchmark/maina.cpp
@@ -0,0 +1,37 @@
+#include "GlobalSettings.hpp"
+#include "Logging.hpp"
+#include "StringUtils.hpp"
+#include "World.hpp"
+
+#include <chrono>
+
+#include <glog/logging.h>
+
+void setBenchmarkSettings()
+{
+    GlobalSettings::get().setNumberOfDiscs(400);
+}
+
+int main(int argc, char** argv)
+{
+    initLogging(argc, argv);
+    setBenchmarkSettings();
+
+    World world;
+    world.setBounds(sf::Vector2f(1000, 1000));
+    world.reinitialize();
+
+    LOG(INFO) << "Starting benchmark";
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < 1000; ++i)
+        world.update(sf::milliseconds(5));
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    LOG(INFO) << "Done";
+    long long ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+    LOG(INFO) << "Elapsed time: " << StringUtils::timeString(ns);
+    LOG(INFO) << "Time per update: " << StringUtils::timeString(ns / 1e3);
+}
