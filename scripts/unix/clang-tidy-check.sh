eeec16001c5e93f78246e96922a0f5c66a63d3bb
Yannik Schroeder <yannik.schroeder@uni-bayreuth.de>
2025-05-28 21:43:56 +0200

    clang-tidy script

---
+++ scripts/unix/clang-tidy-check.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+cd "$(dirname "$0")"
+cd ../..
+
+output=$(run-clang-tidy -p build_release -j 4 -source-filter=".*src/.*|.*test/.*" -header-filter=".*" | tee /dev/stdout)
+
+if echo $output | grep -E "warning:|error:" > /dev/null; then 
+    echo "clang-tidy found warnings/errors. You can rerun it with --fix or fix them yourself."
+    exit 1
+fi 
+
